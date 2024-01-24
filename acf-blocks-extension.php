5041173a09a5786007c99dff1a4467fe7282839d
Marko Krstic <mailme@markokrstic.com>
2024-01-24 06:39:13 +0100

    Update acf-blocks-extension.php

--- acf-blocks-extension.php
+++ acf-blocks-extension.php
@@ -48,19 +48,8 @@ function initialize_acf_block_location_extension()
             public function match($rule, $screen, $field_group)
             {
                 // Define match logic
-                // Example: return true if the current screen is a specific block type
                 return true;
             }
-
-            // Returns an array of possible values for this rule type
-            // public function get_values( $rule ) {
-            //     // Define values for this location rule
-            //     return [
-            //         'block_type_1' => 'Block Type 1',
-            //         'block_type_2' => 'Block Type 2',
-            //         // ... Add more block types as needed
-            //     ];
-            // }
 
             // Returns an array of possible values for this rule type
             public function get_values($rule)
@@ -80,7 +69,6 @@ function initialize_acf_block_location_extension()
 
                 return $specific_domain_blocks;
             }
-
 
             // Ensures this method is static if it's static in the parent class
             public static function get_operators($rule)
